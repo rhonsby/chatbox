98f32299cda19ae6b2c13eec59559ddd3c6503b6
Robert Honsby <rhonsby@gmail.com>
2014-06-02 13:25:12 -0700

    Change port env

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var http = require('http');
 var path = require('path');
 var router = require('./router');
-var chat = require('./lib/chat_server')
+var chat = require('./lib/chat_server');
+
+var port = process.env.PORT || 8080;
 
 var server = http.createServer(function (req, resp) {
   router.router(req, resp);
-}).listen(8080);
+}).listen(port);
 
 chat.createChat(server);
 
-console.log('Server running at http://127.0.0.1:8080/');
+console.log('Server running...');
